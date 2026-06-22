a111655c3adec751c980d0afa1eb759eca204c7b
Misbah Haque <misbahmuneer2003@gmail.com>
2026-06-22 17:16:29 +0500

    Revamp auth pages and footer layout
    Redesign Login and Register views with a full-screen, two-panel auth UI and new inline scoped styles; add Layout = "_Layout" and improve form labels, placeholders, validation summaries and button styles (Shop/Views/Account/Login.cshtml, Shop/Views/Account/Register.cshtml). Replace the simple site footer with a multi-column footer (Shop/Views/Shared/_Layout.cshtml) and remove the old footer CSS rule (Shop/Views/Shared/_Layout.cshtml.css). Also fix a stray closing div in Products/Details.cshtml. Note: build/obj binaries and static web assets were regenerated as part of the build.

--- Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
+++ Shop/obj/Debug/net8.0/Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979d287ba7f9261c687711ffbfe52dfdf6c9454c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b89df3dbc48e8db31ae8ca885077f356c720284")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
