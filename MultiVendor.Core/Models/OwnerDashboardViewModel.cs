6228e75125bf59e5ad66e018fa2c016d874cf2dd
Misbah Haque <misbahmuneer2003@gmail.com>
2026-07-23 12:15:10 +0500

    Update

--- MultiVendor.Core/Models/OwnerDashboardViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MultiVendor.Core.Models;
-
-public class OwnerDashboardViewModel
-{
-    public List<ShopItem> Shops { get; set; } = new();
-    public List<OrderItem> OrderItems { get; set; } = new();
-    public decimal TotalSales { get; set; }
-}
