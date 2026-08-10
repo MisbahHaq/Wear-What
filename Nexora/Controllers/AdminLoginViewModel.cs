96d1369b7410e49cbca7382ed3becceb070e388b
Misbah Haque <misbahmuneer2003@gmail.com>
2026-08-10 11:34:29 +0500

    Update

--- Nexora/Controllers/AdminLoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Nexora.Controllers
-{
-    public class AdminLoginViewModel
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
--- Nexora/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Nexora.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
